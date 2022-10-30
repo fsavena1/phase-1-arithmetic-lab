f24479fe03fe0480ce4176825b9671ba455b2413
frank <franksavena@gmail.com>
2022-10-29 20:56:44 -0500

    commit tree 52e893238cfb1388aa8475f67b00ad1fe67c7e2a

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 8
 
-// Write your code here
+let num2 = 7.75
+
+let multiply = num1 * num2
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+const num3 = 12
+
+const num4 = 3
+
+let mod = num3 / num4
+
+const max = 20
+
+Math.max(1, 2, 20);
